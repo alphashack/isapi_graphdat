57cabf0fcc77131e05a029829e4b61fba65d11ea
David Clark <evil@digitalfivesyndey.com>
2012-08-31 12:46:25 +1000

    debug log in pwd

--- debug.c
+++ debug.c
@@ -1,15 +1,37 @@
 #include "debug.h"
 
-FILE * _debug_fp;
+#define DEBUG_FILE "graphdat_debug.log"
+extern HMODULE MODULE_HANDLE;
+
+FILE * _debug_fp = NULL;
 
 void debug_init()
 {
-	_debug_fp = _fsopen("c:\\tmp\\debug.log", "a", _SH_DENYNO);
+	char * path;
+
+	rsize_t len = MAX_PATH + 1;
+	char exepath[MAX_PATH + 1];
+	if (GetModuleFileNameA(MODULE_HANDLE, exepath, MAX_PATH) != 0)
+	{
+		char * psz = strrchr(exepath, '\\') + 1;
+		len -= (psz - exepath);
+
+		strcpy_s(psz, len, DEBUG_FILE);
+
+		path = exepath;
+	}
+	else
+	{
+		return;
+	}
+
+	_debug_fp = _fsopen(path, "a", _SH_DENYNO);
 }
 
 void debug_term()
 {
-	fclose(_debug_fp);
+	if(_debug_fp != NULL)
+		fclose(_debug_fp);
 }
 
 FILE * debug_fp()
